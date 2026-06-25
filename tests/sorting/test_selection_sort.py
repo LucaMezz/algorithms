078f9eed9c3db2493dfed2dc6222261658e78179
Luca Mezzavilla <lucamezza4@gmail.com>
2026-06-25 18:18:47 +1000

    test: add tests for selection sort

---
+++ tests/sorting/test_selection_sort.py
@@ -0,0 +1,31 @@
+import pytest
+from hypothesis import given
+from hypothesis import strategies as st
+
+from algorithms.sorting import selection_sort
+
+
+class TestSorted:
+    @pytest.mark.parametrize(
+        "values",
+        [
+            [],
+            [0],
+            [3, 2, 1],
+            [1, 2, 3],
+            [-3, -1, -2],
+            [8, 2, 19, 5, 8, 9, 2, 35, 62, 5],
+        ],
+    )
+    def test_sorts(self, values: list[int]) -> None:
+        actual = sorted(values)
+        selection_sort(values)
+        assert values == actual
+
+
+class TestProperties:
+    @given(st.lists(st.integers()))
+    def test_result_is_correct(self, values: list[int]) -> None:
+        actual = sorted(values)
+        selection_sort(values)
+        assert values == actual
