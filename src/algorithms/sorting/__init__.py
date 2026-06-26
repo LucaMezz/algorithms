00ab1d31f687b63fdbb65f64a2e00ecb38e74bda
Luca Mezzavilla <lucamezza4@gmail.com>
2026-06-26 16:29:48 +1000

    feat: implement sort and count inversions

--- src/algorithms/sorting/__init__.py
+++ src/algorithms/sorting/__init__.py
@@ -3,6 +3,7 @@ from algorithms.sorting.insertion_sort import insertion_sort
 from algorithms.sorting.merge_sort import merge_sort
 from algorithms.sorting.quick_sort import quick_sort
 from algorithms.sorting.selection_sort import selection_sort
+from algorithms.sorting.sort_and_count_inversions import sort_and_count_inversions
 
 __all__ = [
     "bubble_sort",
@@ -10,4 +11,5 @@ __all__ = [
     "merge_sort",
     "quick_sort",
     "selection_sort",
+    "sort_and_count_inversions",
 ]
